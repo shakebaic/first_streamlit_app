77d5aba7b1ae4835deed64420a4ae840400eb574
Matt Baic <150735193+shakebaic@users.noreply.github.com>
2023-11-13 16:21:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('Hello world')
+
+streamlit.header('Here is my new resume')
+streamlit.text('Basic')
+streamlit.text('dBase III+')
+streamlit.text('Delphi')
+streamlit.text('Python')
