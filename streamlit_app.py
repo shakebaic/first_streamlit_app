054b37286dab2f52a1c2c81d1d45e4648b5b5f76
Matt Baic <150735193+shakebaic@users.noreply.github.com>
2023-11-13 16:27:19 -0500

    Update streamlit_app.py
    adding dater

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,7 @@ streamlit.text('Delphi')
 streamlit.text('Python')
 streamlit.text('🥑The end')
 streamlit.header('The learning never ends')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
