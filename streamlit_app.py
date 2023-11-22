5ed525a82a281974426c74234459a760905a971a
Matt Baic <150735193+shakebaic@users.noreply.github.com>
2023-11-22 11:06:14 -0500

    Update streamlit_app.py
    add a try / except block

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,18 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?' ,'Kiwi')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+      streamlit.error("Please select a fruit")
+  else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pandas.json_normalized(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+
+execpt URLError as e:
+    streamlit.error()
+
 streamlit.write('The user entered', fruit_choice)
 
 #import requests
