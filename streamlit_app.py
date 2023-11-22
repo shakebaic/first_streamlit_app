60c301054e9d89765fbc7993cf23fb686a567cc7
Matt Baic <150735193+shakebaic@users.noreply.github.com>
2023-11-22 11:26:23 -0500

    Update streamlit_app.py
    added back_from_function to write command

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,10 +43,10 @@ try:
      back_from_function = get_fruityvice_data(fruit_choice)
      streamlit.dataframe(back_from_function)
 
-#streamlit.write('The user entered', fruit_choice)
+#streamlit.write('The user entered', back_from_function)
 
 #import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # take the json version of the response and normalize it
 #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
