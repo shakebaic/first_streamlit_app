06baa1f255645fb2064a6892ed0cd2d6447d2923
Matt Baic <150735193+shakebaic@users.noreply.github.com>
2023-11-22 09:35:19 -0500

    Update streamlit_app.py
    importing snowflake.connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 
 streamlit.title('Hello world')
-
 streamlit.header('New restaurant offering food!')
 streamlit.text('Meat')
 streamlit.text('Fruit')
@@ -14,8 +13,6 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
-
-
 
 # ADd a pick list here so they can pick the fruit they want to include
 fruits_selected = streamlit.multiselect("Pick some fruit:", list(my_fruit_list.index),['Avocado','Strawberries'])
@@ -37,3 +34,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it to the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
+import snowflake.connector
