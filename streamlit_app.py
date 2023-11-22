fca54711676a2d4e3b54a21ea0e0912d9baaaa65
Matt Baic <150735193+shakebaic@users.noreply.github.com>
2023-11-22 11:34:12 -0500

    Update streamlit_app.py
    added except back in

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,9 @@ try:
      #back_from_function = get_fruityvice_data(fruit_choice)
      #streamlit.dataframe(back_from_function)
 
+except URLError as e:
+    streamlit.error()
+  
 streamlit.write('The user entered', back_from_function)
 
 #import requests
