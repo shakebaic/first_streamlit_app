4fd10deda374f4fb8081fb805eae8ff9491798dd
Matt Baic <150735193+shakebaic@users.noreply.github.com>
2023-11-22 10:39:01 -0500

    Update streamlit_app.py
    misspelled select

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ import snowflake.connector
 # lesson 2.12
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("seelct * from fruit_load_list")
+my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
